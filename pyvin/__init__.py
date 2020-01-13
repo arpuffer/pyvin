05884af83a4140686dad4e8dbbaf5fa43d861756
Alex Puffer <alex.puffer@gmail.com>
2020-01-12 18:44:47 -0800

    Added HTML documentation, get_version to modue at parent level

--- pyvin/__init__.py
+++ pyvin/__init__.py
@@ -5,3 +5,8 @@ from .pyvin import (VIN,
                     PASS)
 from .errors import VINError
 from .utils import clean_vins, validate_vin
+
+_VERSION = '0.0.1'
+
+def get_version():
+    return _VERSION
