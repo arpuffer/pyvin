af1ea42af23fe8db7e63c96e353cf82d6b3a783a
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-04-28 13:49:48 -0400

    More version stuff

--- setup.py
+++ setup.py
@@ -1,29 +1,4 @@
 """setup pyvin library"""
+from setuptools import setup
 
-from setuptools import setup, find_packages
-
-import pkg_resources  # part of setuptools
-VERSION = pkg_resources.require("pyvin")[0].version
-
-PKG = 'pyvin'
-
-
-setup(
-    name=PKG,
-    version=VERSION,
-    author="Alex Puffer",
-    description="Python VIN decoder",
-    url="https://github.com/arpuffer/pyvin",
-    packages=find_packages(),
-    provides=[PKG],
-    install_requires=[
-        'requests~=2.25.1',
-    ],
-    test_suite='tests',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup()
